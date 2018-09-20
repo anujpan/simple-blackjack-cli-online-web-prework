b4efe047b13c9eabb8a87a10b12c60dae58aa1fd
Anuj Panchal <anujpan1@gmail.com>
2018-09-20 17:02:11 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,10 +30,12 @@ end
 
 def hit?(card_total)
   prompt_user
-  input = get_user_input
+  get_answer
 end
 
-def check_confidence(card_total)
+def get_answer(card_total)
+  input = get_user_input
+
   if input == "h"
     card_total += deal_card
   elsif input == "s" && card_total == 21
