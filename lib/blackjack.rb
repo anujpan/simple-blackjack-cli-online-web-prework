5d39fd76c5843f8d1001a11c57c4b089da565f22
Anuj Panchal <anujpan1@gmail.com>
2018-09-20 16:53:52 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,8 +42,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  # code invalid_command here
-  puts "invalid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
